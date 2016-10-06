27f1d0ec0c4db0829086a82fbd806266516e71b8
Anastasia Krasenkova <krasenkova.a@gmail.com>
2016-10-07 00:11:11 +0300

    changes in TestData Answer Question

---
+++ src/FileManipulator.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+
+public class FileManipulator {
+    private static FileManipulator instance;
+
+    private FileManipulator() {
+    }
+
+    public static FileManipulator getInstance() {
+        if (instance == null) {
+            instance = new FileManipulator();
+        }
+        return instance;
+    }
+
+    public ArrayList<String> readFileToStringList (String fileName) {
+        ArrayList<String> returnList = new ArrayList<>();
+
+        try (BufferedReader reader = new BufferedReader(
+                new InputStreamReader(
+                        new FileInputStream(fileName), StandardCharsets.UTF_8))){
+            String line;
+            while ((line = reader.readLine()) != null) {
+                returnList.add(line);
+            }
+        } catch (IOException e) {
+            System.out.println("Cannot to read from file " + fileName);
+        }
+
+        return returnList;
+    }
+
+}
--- src/TestData.java
+++ src/TestData.java
@@ -1,36 +1,62 @@
 
 import java.lang.String;
 import java.util.ArrayList;
-import java.util.Arrays;
 
 /**
- * In this Class will be created hashMap ??? or ArrayList with Questions and Answers Pairs
+ * The class containing data for a particular test
  */
+
 public class TestData {
-    //int testId; нужен ли он?
+    //int testId; do we need it?
     private String testName;
     private String testDescription;
     private ArrayList<Question> questions;
 
-    //Construktor makes a new HashMap with Pairs of Question(String)-Answers(Answer)
-    public TestData(int fileIndex){
-/**
- * fileIndex - номер файла, откуда берем тест. Если тестов будет несколько, то файлы, допустим будут называться
- * test1.txt, test2.txt
- */
-        this.testName = "We get test name from some file";
-        this.testDescription = "We get test description from some file";
-        this.questions = new ArrayList<Question>();
+    public String getTestName() {
+        return testName;
+    }
 
-        // now we get all questions for the test from some file
-        //test for first Question
-        ArrayList<Answer> listOfAnswersForTheQuestion = new ArrayList<Answer>( // we put all answers for the question into ArrayList
-                Arrays.asList(
-                        new Answer("Putin", true),
-                        new Answer("Navalniy", false),
-                        new Answer("Obama", false)
-                        ));
-        questions.add(new Question("Who is the next President of RF", listOfAnswersForTheQuestion)); // we put the question into ArrayList of questions
+    public void setTestName(String testName) {
+        this.testName = testName;
+    }
+
+    public String getTestDescription() {
+        return testDescription;
+    }
+
+    public void setTestDescription(String testDescription) {
+        this.testDescription = testDescription;
+    }
+
+    public ArrayList<Question> getQuestions() {
+        return questions;
+    }
+
+    public void setQuestions(ArrayList<Question> questions) {
+        this.questions = questions;
+    }
+
+    public TestData(int fileIndex){
+        FileManipulator fileManipulator = FileManipulator.getInstance();
+        ArrayList<String> testData = fileManipulator.readFileToStringList("./tests/test" + fileIndex + ".txt");
+
+        this.testName = testData.remove(0);
+        this.testDescription = testData.remove(0);
+        this.questions = new ArrayList<>();
+
+        for (String questionString : testData) {
+            String[] parsedQuestionData = questionString.split("\\|\\|\\|");
+            ArrayList<Answer> answers = new ArrayList<>();
+            boolean isCorrect = true;
+            for (int i = 1; i < parsedQuestionData.length; i++) {
+                if (parsedQuestionData[i].equals("divider25745406472")) {
+                    isCorrect = false;
+                    continue;
+                }
+                answers.add(new Answer(parsedQuestionData[i],isCorrect));
+            }
+            questions.add(new Question(parsedQuestionData[0],answers));
+        }
     }
 
 }
